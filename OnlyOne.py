971bb244c129132d5f29327cd1b29885f3698bda
HEPSTAR <admin@hepstar.kr>
2015-09-03 01:46:23 +0900

    Layer7 CTF 2015 OnlyOne Python Code

---
+++ OnlyOne.py
@@ -0,0 +1,12 @@
+encrypt = [ 0x25,0x3e,0x31,0x51,0x26,0x36,0x3a,0x2e,0x55,0x1c ]
+outp = "i^vbopbsbk"
+inp = "layerseven"
+Flag = ""
+
+for i in range(len(encrypt)):
+    tmp = encrypt[i] ^ ord(outp[i]) ^ ord(outp[i])
+    Flag += chr(tmp ^ ord(inp[i]))
+print Flag
+
+for i in range(len(encrypt)):
+    print chr(encrypt[i])
